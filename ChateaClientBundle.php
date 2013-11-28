424cf508b37bad0c2ad24fc549d5f6a3129d9f28
jdeveloper <jdeveloper.inxenio@gmail.com>
2013-11-28 16:04:48 +0100

    Removed Authentication provider

--- ChateaClientBundle.php
+++ ChateaClientBundle.php
@@ -4,13 +4,7 @@ namespace Ant\Bundle\ChateaClientBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Ant\Bundle\ChateaClientBundle\DependencyInjection\Factory\SecurityFactory;
 
 class ChateaClientBundle extends Bundle
 {
-	public function build(ContainerBuilder $container)
-    {
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new SecurityFactory());
-    }
 }
--- Security/User/ChateaUserProviderInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Ant\Bundle\ChateaClientBundle\Security\User;
-
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-
-interface ChateaUserProviderInterface extends UserProviderInterface
-{
-    public function loadUser($username, $password);
-}
