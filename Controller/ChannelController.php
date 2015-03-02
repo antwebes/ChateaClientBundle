c671bd8dfac47e84763dc7dd0ea457a01359a423
jdeveloper <jdeveloper.inxenio@gmail.com>
2015-03-02 09:45:30 +0100

    Added layout for chatea client bundle

--- Controller/ChannelController.php
+++ Controller/ChannelController.php
@@ -6,6 +6,7 @@ use Ant\Bundle\ChateaClientBundle\Form\CreateChannelType;
 use Ant\Bundle\ChateaClientBundle\Security\Authentication\Annotation\APIUser;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 
 class ChannelController extends Controller
@@ -28,7 +29,7 @@ class ChannelController extends Controller
                     $channelManager->save($channel);
                     return $this->render('ChateaClientBundle:Channel:registerSuccess.html.twig');
                 }catch(\Exception $e){
-
+                    $this->addErrorsToForm($e, $form);
                 }
             }
         }
@@ -37,5 +38,58 @@ class ChannelController extends Controller
             'form' => $form->createView(),
             'from_has_error' => count($form->getErrors()) > 0,
         ]);
+    }
+
+    protected function addErrorsToForm($e, $form)
+    {
+        $translator = $this->get('translator');
+
+        if ($this->isJson($e->getMessage())){
+            $serverError = json_decode($e->getMessage());
+            if(is_object($serverError->errors)){
+                $errors = json_decode($e->getMessage(), true);
+                $this->fillForm($errors['errors'], $form);
+            }else{
+                $form->addError(new FormError($translator->trans('%%error%%', array('%%error%%' => $serverError->errors))));
+            }
+        } else {
+            //llega un string de error proveniente de la libreria
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     *
+     * @param array $errors errors of the form
+     * @param Form $form form where we go to include the errors
+     */
+    private function fillForm($errors, $form)
+    {
+        $translator = $this->get('translator');
+
+        foreach ($errors as $field => $message) {
+
+            if ($form->has($field)){
+                if (isset($message['message'])){
+                    $form->get($field)->addError(new FormError($translator->trans('%%error%%', array('%%error%%' => $message['message']))));
+                }else{
+                    //in this case the message is a form field within another field.
+                    $this->fillForm($message, $form->get($field));
+                }
+            }else{
+                //receive an error from the library
+                //the message can to be a string :"This form should not contain extra fields." For this reason I include this if
+                $messageString = (isset($message['message']) ? $message['message']: $message);
+                $form->addError(new FormError($translator->trans('%%error%%', array('%%error%%' => $field . '->'.$messageString))));
+            }
+        }
+    }
+
+    private function isJson($string)
+    {
+        return (is_string($string) && (
+                is_object(json_decode($string)) || is_array(json_decode($string))
+            )
+        );
     }
 }
