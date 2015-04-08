4b8f2ac36052d066bd3ae75c90feca0b42e9efcf
jdeveloper <jdeveloper.inxenio@gmail.com>
2015-04-08 15:52:45 +0200

    Changed channel slug generation

--- Resources/public/js/channelSlug.js
+++ Resources/public/js/channelSlug.js
@@ -1,7 +1,8 @@
 $(document).ready( function() {
-    $('input[data-id="channel_name"]').stringToSlug({
-        callback: function(text){
-            $('input[data-id="channel_irc_channel"]').val("#"+text)
-        }
+    $('input[data-id="channel_name"]').bind('keyup keydown blur', function(){
+        var text = $(this).val();
+        var regex = /[,\cG]/gm; //detecta espacions, caracter "," y ctr+G //los que no permite contener IRC en canal
+
+        $('input[data-id="channel_irc_channel"]').val("#"+text.replace(regex, '').replace(/[\s]/gm, '-'));
     });
 });
