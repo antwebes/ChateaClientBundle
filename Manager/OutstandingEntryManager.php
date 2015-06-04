fab8dd8dbafa799385da81d01d51ea7fe51651c4
jdeveloper <jdeveloper.inxenio@gmail.com>
2015-06-04 11:15:31 +0200

    Added OutstandingEntryManager

---
+++ Manager/OutstandingEntryManager.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Created by Javier Fernández Rodríguez for Ant-Webs S.L.
+ * User: Xabier <jbier@gmail.com>
+ * Date: 3/12/14 - 16:34
+ */
+
+namespace Ant\Bundle\ChateaClientBundle\Manager;
+
+use Ant\Bundle\ChateaClientBundle\Api\Model\OutstandingEntry;
+use Ant\Bundle\ChateaClientBundle\Api\Util\Pager;
+use Ant\Bundle\ChateaClientBundle\Api\Util\Command;
+
+
+/**
+ * Class OperatorManager
+ * @package Ant\Bundle\ChateaAdminBundle\Manager
+ */
+class OutstandingEntryManager extends BaseManager
+{
+    protected $limit;
+
+    public function hydrate(array $item = null)
+    {
+        $outstandingEntry = new OutstandingEntry();
+
+        $outstandingEntry->setManager($this);
+        $outstandingEntry->setId(array_key_exists('id',$item)?$item['id'] : null);
+        $outstandingEntry->setInvalidated(array_key_exists('invalidated',$item)?$item['invalidated'] : false);
+        $outstandingEntry->setUntil(array_key_exists('until',$item) ? new \DateTime($item['until']) : null);
+        $outstandingEntry->setFrom(array_key_exists('from',$item) ? new \DateTime($item['from']) : null);
+        $outstandingEntry->setCreatedAt(array_key_exists('created_at',$item) ? new \DateTime($item['created_at']) : null);
+
+        if(isset($item['outstander']) && isset($item['outstander']['id'])){
+            $outstander = $this->get('UserManager')->hydrate($item['outstander']);
+            $outstandingEntry->setOutstander($outstander);
+        }
+
+        if(isset($item['resource']) && isset($item['resource']['id'])){
+            $resource = $this->get('UserManager')->hydrate($item['resource']);
+            $outstandingEntry->setResource($resource);
+        }
+
+        return $outstandingEntry;
+    }
+
+    public function setLimit($limit)
+    {
+        $this->limit = $limit;
+    }
+
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    public function findById($id)
+    {
+        throw new \Exception('This function is not enabled yet');
+    }
+
+    public function findAll($page = 1, array $filters = null, $limit = null, array $order = null)
+    {
+        if (!$limit) {
+            $limit = $this->limit;
+        }
+
+        $command = new Command('findOperators',array('filter' => $filters, 'order' => $order));
+        return new Pager($this,$command, $page, $limit);
+    }
+
+    public function findOperatorsType()
+    {
+        return $this->getManager()->findOperatorsType();
+    }
+    public function save(&$object)
+    {
+        if(!($object instanceof Operator)){
+            throw new \InvalidArgumentException('The parameter have been of type Operator');
+        }
+
+        $newOperator = $this->getManager()->saveOperators(
+            $object->getUsername(),
+            $object->getPassword(),
+            $object->getType(),
+            $object->getHostname()
+        );
+        $object = $this->hydrate($newOperator);
+    }
+
+    /**
+     * @param $object
+     * @throws \Exception This function is not enabled yet
+     */
+    public function update(&$object)
+    {
+        throw new \Exception('This function is not enabled yet');
+    }
+
+    /**
+     * @param $object_id
+     */
+    public function delete($object_id)
+    {
+        $this->getManager()->deleteOperators($object_id);
+    }
+
+    public function getModel()
+    {
+        return 'Ant\Bundle\ChateaClientBundle\Api\Model\OutstandingEntry';
+    }
+}
