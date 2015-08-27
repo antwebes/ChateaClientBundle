8e95b91dde148176f5304b8c4fdd2c218330e087
jota_develope <jdeveloper.inxenio@gmail.com>
2015-08-27 09:50:32 +0200

    Registered well Ant\Bundle\ChateaClientBundle namespace for Doctrine Annotations for tests

--- autoload.php
+++ autoload.php
@@ -8,7 +8,6 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/vendor/autoload.php';
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
+AnnotationRegistry::registerAutoloadNamespace('Ant\Bundle\ChateaClientBundle', __DIR__ . '/../../..');
 
 return $loader;
